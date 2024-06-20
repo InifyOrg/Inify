ebdb6f40496b95fcdb63fda086830549db90bd46
Sergey Svist <sergeysvist557@gmail.com>
2024-06-20 12:12:55 +0300

    walletsMS init and architecture

---
+++ WalletsMS/WalletsMS.Host/Controllers/WalletsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WalletsMS.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletsController : ControllerBase
+    {
+    }
+}
