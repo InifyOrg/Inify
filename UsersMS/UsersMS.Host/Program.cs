68156d32ffdf6ec5f09f123c2aaea27c1eee8831
Sergey Svist <sergeysvist557@gmail.com>
2024-06-29 17:23:32 +0300

    fix json encoding error

--- UsersMS/UsersMS.Host/Program.cs
+++ UsersMS/UsersMS.Host/Program.cs
@@ -5,8 +5,12 @@ using UsersMS.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ UsersMS/UsersMSTestProject/Program.cs
@@ -0,0 +1,8 @@
+﻿using UsersMS.Client;
+using UsersMS.Contracts;
+
+UsersMsClient client = new UsersMsClient();
+
+UserDTO user = await client.GetUserByID(1);
+
+Console.WriteLine($"{user.Id} = {user.Email}");
