61b2e35313cf5f7637917672f5fc2c99c2db9ca6
Sergey Svist <sergeysvist557@gmail.com>
2024-07-11 13:11:13 +0300

    init tokensMs

---
+++ TokensMS/TokensMS.Host/Controllers/TokensController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokensMS.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokensController : ControllerBase
+    {
+
+    }
+}
