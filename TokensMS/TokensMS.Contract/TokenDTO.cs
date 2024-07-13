eb29fb76697de112665cc69963e1427b9cb5a569
Sergey Svist <sergeysvist557@gmail.com>
2024-07-13 15:41:49 +0300

    init TokensMs.Contracts with test version of TokenDTO

---
+++ TokensMS/TokensMS.Contract/TokenDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokensMS.Contract
+{
+    public class TokenDTO
+    {
+        //не конечная дто, делать по ней бд нельзя, просто наброски чтобы понимать на чем строить бд
+        public long Id {  get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string? Address { get; set; }
+        public int Decimals { get; set; }
+        public string? PlatformSymbol { get; set; }
+        public string WalletType { get; set; }
+    }
+}
