8df23d955a6770dad2808df5a59c59dbaa9bafe8
Sergey Svist <sergeysvist557@gmail.com>
2024-07-18 16:27:38 +0300

    finish: tokensMs data layer

--- TokensMS/TokensMS.Infrastructure/DataLayer/TokensDataLayer.cs
+++ TokensMS/TokensMS.Infrastructure/DataLayer/TokensDataLayer.cs
@@ -1,22 +1,69 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TokensMS.Infrastructure.Domain.DbCtx;
 using TokensMS.Infrastructure.Domain.Entities;
 
 namespace TokensMS.Infrastructure.DataLayer
 {
     public class TokensDataLayer : ITokensDataLayer
     {
-        public Task<Platform> AddPlatform(Platform addPlatformDTO)
+        public async Task<Platform> AddPlatform(Platform addPlatformDTO)
         {
-            throw new NotImplementedException();
+            using (TokensMsDbContext db = new TokensMsDbContext())
+            {
+                db.Platforms.Add(addPlatformDTO);
+                await db.SaveChangesAsync();
+
+                return addPlatformDTO;
+            }
         }
 
-        public Task<WalletType> AddWalletType(WalletType newWalletType)
+        public async Task<Token> AddToken(Token newToken)
         {
-            throw new NotImplementedException();
+            using (TokensMsDbContext db = new TokensMsDbContext())
+            {
+                newToken.WalletType = await db.WalletTypes.Where(wt => wt.Title == newToken.WalletType.Title).FirstOrDefaultAsync();
+                newToken.Platform = await db.Platforms.Where(wt => wt.Slug == newToken.Platform.Slug).FirstOrDefaultAsync();
+                db.Tokens.Add(newToken);
+                await db.SaveChangesAsync();
+
+                return newToken;
+            }
+        }
+
+        public async Task<WalletType> AddWalletType(WalletType newWalletType)
+        {
+            using (TokensMsDbContext db = new TokensMsDbContext())
+            {
+                db.WalletTypes.Add(newWalletType);
+                await db.SaveChangesAsync();
+
+                return newWalletType;
+            }
+        }
+
+        public async Task<List<Token>> GetAll()
+        {
+            using (TokensMsDbContext db = new TokensMsDbContext())
+            {
+                List<Token> tokens = await db.Tokens.ToListAsync();
+
+                return tokens != null ? tokens : new List<Token>();
+            }
+        }
+
+        public async Task<List<Token>> GetAllByWalletType(string walletType)
+        {
+            using (TokensMsDbContext db = new TokensMsDbContext())
+            {
+                List<Token> tokens = await db.Tokens.Where(t => t.WalletType.Title == walletType).ToListAsync();
+
+                return tokens != null ? tokens : new List<Token>();
+            }
         }
     }
 }
