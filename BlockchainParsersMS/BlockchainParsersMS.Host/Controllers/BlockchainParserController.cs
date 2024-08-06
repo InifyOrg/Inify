d6685a1bba20cb0b08bb94745688bf602f567baa
Sergey Svist <sergeysvist557@gmail.com>
2024-08-06 20:26:39 +0300

    add: blockchainParsersMs infrastructure

---
+++ BlockchainParsersMS/BlockchainParsersMS.Host/Controllers/BlockchainParserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlockchainParsersMS.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlockchainParserController : ControllerBase
+    {
+
+        [HttpGet("parseManyByUserId/{userId}")]
+        public async Task<IActionResult> parseManyByUserId(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("parseOneByAddress/{address}")]
+        public async Task<IActionResult> parseOneByAddress(string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
