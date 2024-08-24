f426a841382220fe12149c064014754f5f706d45
Sergey Svist <sergeysvist557@gmail.com>
2024-08-24 15:23:12 +0300

    add: apiGateway with architecture

---
+++ ApiGateway/ApiGateway.Host/Controllers/UsersMsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersMsController : ControllerBase
+    {
+    }
+}
---
+++ ApiGateway/ApiGateway.Host/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
